fa2ecf04640011fe843a9b350415545cfde5c67d
Li-Tian <tianli@neo.org>
2018-07-23 13:59:27 +0800

    first commit

---
+++ DbgViewTR/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbgViewTR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TR.enter();
+            doSomething();
+            TR.exit();
+        }
+
+        static int doSomething()
+        {
+            TR.enter();
+            TR.log("Do something.");
+            return TR.exit(1);
+        }
+    }
+}
