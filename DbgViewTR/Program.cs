4c21c84e77467d863c3756893719ef4a63e88657
Li-Tian <tianli@neo.org>
2018-07-30 17:30:40 +0800

    Add Context ID into log

--- DbgViewTR/Program.cs
+++ DbgViewTR/Program.cs
@@ -11,15 +11,37 @@ namespace DbgViewTR
         static void Main(string[] args)
         {
             TR.Enter();
-            doSomething();
+            DoSomething();
+            IndentContext ic = TR.SaveContextAndShuffle();
+            Task t = DoSomethingAsync();
+            TR.RestoreContext(ic);
+            TR.Log(t.Id);
+            Task t2 = Task.Run(() => DoSomething());
+            TR.Log(t.Id);
+            t.Wait();
+            t2.Wait();
             TR.Exit();
         }
 
-        static int doSomething()
+        static int DoSomething()
         {
             TR.Enter();
             TR.Log("Do something.");
             return TR.Exit(1);
+        }
+
+        static async Task DoSomethingAsync()
+        {
+            TR.Enter();
+            IndentContext ic = TR.SaveContextAndShuffle();
+            await Task.Run(()=>{
+                TR.Enter();
+                TR.Log("Do something async.");
+                TR.Exit();
+            });
+            TR.RestoreContext(ic);
+            TR.Log();
+            TR.Exit();
         }
     }
 }
